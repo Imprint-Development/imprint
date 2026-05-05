b4a7709dbfaae723714df2ae12bfafaa829cb99b
Christoph Wedenig <christoph.wedenig@dynatrace.com>
2026-05-05 12:32:36 +0200

    chore(migrations): remove bad 0001 migration re-introduced by cherry-pick

--- drizzle/0001_grades_unique_indexes.sql
+++
@@ -1,2 +0,0 @@
-CREATE UNIQUE INDEX "grades_per_checkpoint_unique" ON "grades" ("student_id","category_id","checkpoint_id") WHERE "checkpoint_id" IS NOT NULL;--> statement-breakpoint
-CREATE UNIQUE INDEX "grades_standalone_unique" ON "grades" ("student_id","category_id") WHERE "checkpoint_id" IS NULL;
